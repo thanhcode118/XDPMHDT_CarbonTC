28146a6ae81c4972b1d560faca1622f782591006
Phúc Đại <dai742004.dn@gmail.com>
2025-10-07 14:08:02 +0700

    feat : Thêm Specifications cho các AggregateRoot

---
+++ CarbonTC.Marketplace.Service/Domain/Specifications/InventorySpecifications.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+
+namespace Domain.Specifications
+{
+    public static class InventorySpecifications
+    {
+        public static bool HasSufficientAvailable(CreditInventory inventory, decimal requiredAmount)
+        {
+            return inventory.AvailableAmount >= requiredAmount;
+        }
+
+        public static bool HasSufficientListed(CreditInventory inventory, decimal requiredAmount)
+        {
+            return inventory.ListedAmount >= requiredAmount;
+        }
+
+        public static bool HasSufficientLocked(CreditInventory inventory, decimal requiredAmount)
+        {
+            return inventory.LockedAmount >= requiredAmount;
+        }
+    }
+}
