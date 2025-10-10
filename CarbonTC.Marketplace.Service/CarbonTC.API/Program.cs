3afe56b8509bda9d97a8806c84fa33748bcfccbf
Phúc Đại <dai742004.dn@gmail.com>
2025-10-10 09:07:45 +0700

    feat: Thêm GlobalException

--- CarbonTC.Marketplace.Service/CarbonTC.API/Program.cs
+++ CarbonTC.Marketplace.Service/CarbonTC.API/Program.cs
@@ -1,6 +1,9 @@
 
 using Application;
+using CarbonTC.API.ExceptionHandling;
 using Infrastructure;
+using Microsoft.AspNetCore.Http.Features;
+using System.Diagnostics;
 
 namespace CarbonTC.API
 {
@@ -11,16 +14,33 @@ namespace CarbonTC.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddProblemDetails();
+            builder.Services.AddProblemDetails(options =>
+            {
+                options.CustomizeProblemDetails = context =>
+                {
+                    context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+                    context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+                    Activity? activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+                    context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
+                };
+            });
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+
             builder.Services.AddInfrastructure(builder.Configuration);  
             builder.Services.AddApplication();  
 
             var app = builder.Build();
+
+            app.UseExceptionHandler();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
