889206696d457da102af00bdbe2785438e321ce4
Nguyễn Hà Thanh <nguyenhathanh844@gmail.com>
2025-11-07 14:24:06 +0700

    feat: Thanh done frontend features.

--- CarbonTC_Frontend/src/api/axios.js
+++ CarbonTC_Frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5183/api';
+const API_BASE_URL = 'http://localhost:5001/api';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
